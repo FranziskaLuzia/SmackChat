b4a6a6b1fe43df1703a6a2bc76404c59d6737980
FranziskaLuzia <franziskakammerl@Franziskas-MacBook-Pro.local>
2017-09-24 13:14:52 -0400

    AvatarPicker, Select Avatars, Sizing

--- SmackChat/Controller/CreateAccountVC.swift
+++ SmackChat/Controller/CreateAccountVC.swift
@@ -26,6 +26,13 @@ class CreateAccountVC: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
 
     // Actions
     @IBAction func closePressed(_ sender: Any) {
